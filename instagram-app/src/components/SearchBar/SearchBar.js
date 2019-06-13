0ab160e0f9b430fdaccb29ad6d6c5dc423a4b2ef
Matt Hagner <web@matthagner.com>
2019-06-12 20:07:01 -0500

    Add icons to search bar

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { FaInstagram, FaRegCompass, FaRegHeart, FaRegUser } from "react-icons/fa";
 
 export default function Searchbar() {
   return (
     <header>
-      <a href="./home">I logo</a>
-      <span>Vertical Break</span>
+      <FaInstagram size="3rem" />
+      <div>Vertical break</div>
       <h1>Instagram</h1>
 
       <input placeholder="Search" />
 
       <nav>
         <ul>
-          <li>Some icon</li>
-          <li>Another Icon</li>
-          <li>A third icon</li>
+          <li>
+            <FaRegCompass />
+          </li>
+          <li><FaRegHeart /></li>
+          <li><FaRegUser /></li>
         </ul>
       </nav>
     </header>
