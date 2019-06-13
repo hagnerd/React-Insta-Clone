53c526e4e143860ebe023cc1ec2abaacc9495a2e
Matt Hagner <web@matthagner.com>
2019-06-12 19:13:13 -0500

    Add rough SearchBar component

---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Searchbar() {
+  return (
+    <header>
+      <a href="./home">I logo</a>
+      <span>Vertical Break</span>
+      <h1>Instagram</h1>
+
+      <input placeholder="Search" />
+
+      <nav>
+        <ul>
+          <li>Some icon</li>
+          <li>Another Icon</li>
+          <li>A third icon</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
