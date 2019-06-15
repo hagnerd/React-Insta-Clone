51d2f026ceda839b7c3e7d2989c4007314cfa0ae
Matt Hagner <web@matthagner.com>
2019-06-15 11:29:32 -0500

    Add componentDidMount lifecycle method and state

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -3,15 +3,31 @@ import dummyData from "./dummy-data";
 import SearchBar from "./components/SearchBar/SearchBar";
 import PostContainer from "./components/PostContainer/PostContainer";
 
-function App() {
-  return (
-    <>
-      <SearchBar />
-      <main className="w-7/12 mx-auto">
-        <PostContainer posts={dummyData} />
-      </main>
-    </>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      posts: [],
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      posts: dummyData,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <SearchBar />
+        <main className="w-7/12 mx-auto">
+          <PostContainer posts={this.state.posts} />
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
