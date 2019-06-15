2cdcbd0d76ff9fa83d2eef4356e96e437ffa6f2e
Matt Hagner <web@matthagner.com>
2019-06-15 12:52:21 -0500

    Add ability to add new comment

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -27,6 +27,31 @@ export default class CommentSection extends React.Component {
     });
   }
 
+  handleChange = event => {
+    this.setState({
+      commentText: event.target.value,
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const newComment = {
+      id: this.state.comments.length + 1,
+      username: 'currentlyLoggedInUser',
+      text: this.state.commentText,
+    }
+
+    this.setState(prevState => ({
+      comments: prevState.comments.concat([newComment]),
+      commentText: "",
+    }), () => {
+      // TODO: Remove this before submitting 
+      console.log(`Sending new comment to the server`)
+      console.log(newComment);
+    });
+  }
+
   render() {
     return (
       <>
@@ -38,14 +63,17 @@ export default class CommentSection extends React.Component {
         <h5 className="text-xs text-gray-600 my-2">{this.props.timestamp}</h5>
         <div className="border-b border-gray-300 w-full mx-auto" />
         
-        <form className="flex justify-between h-10 mt-4 items-center">
+        <form onSubmit={this.handleSubmit} className="flex justify-between h-10 mt-4 items-center">
           <input
+            value={this.state.commentText}
+            name="commentText"
+            onChange={this.handleChange}
             placeholder="Add a comment..."
-            className="text-gray-700 text-sm"
+            className="text-gray-700 text-sm h-full w-3/4"
           />
           <button 
             disabled={this.state.commentText === ""}
-            className={`${this.state.commentText !== "" ? "text-blue-400" : "text-blue-300 opacity-75 cursor-not-allowed"}`}
+            className={`${this.state.commentText !== "" ? "text-blue-600 font-bold" : "text-blue-300 opacity-75 cursor-not-allowed"}`}
           >Post</button>
         </form>
 
