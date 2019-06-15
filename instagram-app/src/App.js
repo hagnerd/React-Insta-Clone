4cfcd881b8c00c8aa2408356c39db8e99bfefa92
Matt Hagner <web@matthagner.com>
2019-06-15 11:23:52 -0500

    Add components to App

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import dummyData from "./dummy-data";
 import SearchBar from "./components/SearchBar/SearchBar";
+import PostContainer from "./components/PostContainer/PostContainer";
 
 function App() {
   return (
     <>
       <SearchBar />
+      <main className="w-7/12 mx-auto">
+        <PostContainer posts={dummyData} />
+      </main>
     </>
   );
 }
