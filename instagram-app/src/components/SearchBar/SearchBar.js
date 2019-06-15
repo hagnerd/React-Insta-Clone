58bf30333e52f920632a2bae8a6158b9add530ae
Matt Hagner <web@matthagner.com>
2019-06-15 11:50:23 -0500

    Convert SearchBar to class component

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -6,33 +6,47 @@ import {
   FaRegUser
 } from "react-icons/fa";
 
-export default function Searchbar() {
-  return (
-    <header className="flex justify-around h-20 items-center border-b border-gray-300 w-5/6 mx-auto">
-      <div className="flex h-full w-1/4 items-center">
-        <FaInstagram size="2.5rem" color="#2d3748" />
-        <div className="border-l border-gray-600 h-10 mx-4" />
-        <h1>Instagram</h1>
-      </div>
+export default class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
 
-      <input
-        className="w-1/4 self-center border border-gray-400 rounded py-1 text-center bg-gray-100"
-        placeholder="Search"
-      />
+    this.state = {
+      searchValue: '',
+    }
+  }
 
-      <nav className="w-1/6">
-        <ul className="flex justify-around w-full">
-          <li>
-            <FaRegCompass size="1.5rem" color="#4a5568" />
-          </li>
-          <li>
-            <FaRegHeart size="1.5rem" color="#4a5568" />
-          </li>
-          <li>
-            <FaRegUser size="1.5rem" color="#4a5568" />
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+  render() {
+
+    return (
+      <header className="flex justify-around h-20 items-center border-b border-gray-300 w-5/6 mx-auto">
+        <div className="flex h-full items-center">
+          <FaInstagram size="2.5rem" color="#2d3748" />
+          <div className="border-l border-gray-600 h-10 mx-4" />
+          <h1>Instagram</h1>
+        </div>
+
+        <form className="w-1/3 self-center">
+          <input
+            className="w-full border border-gray-400 rounded py-1 text-center bg-gray-100"
+            placeholder="Search"
+            value={this.state.searchValue}
+          />
+        </form>
+
+        <nav className="w-1/6">
+          <ul className="flex justify-around w-full">
+            <li>
+              <FaRegCompass size="1.5rem" color="#4a5568" />
+            </li>
+            <li>
+              <FaRegHeart size="1.5rem" color="#4a5568" />
+            </li>
+            <li>
+              <FaRegUser size="1.5rem" color="#4a5568" />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
 }
