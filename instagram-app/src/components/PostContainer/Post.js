a4188ddd5988f5e42f8631a92a9a47a5fc2e0c57
Matt Hagner <web@matthagner.com>
2019-06-17 21:49:44 -0500

    Fix issue with icon sizing

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaRegHeart, FaHeart, FaRegComment } from "react-icons/fa";
-import { MdMoreHoriz } from 'react-icons/md'
+import { MdMoreHoriz } from "react-icons/md";
 import PropTypes from "prop-types";
 import CommentSection from "../CommentSection/CommentSection";
 import Comment from "../CommentSection/Comment";
@@ -14,45 +14,42 @@ export default function Post({
   comments,
   doesCurrentUserLike,
   toggleLike,
-  currentlyLoggedInUser,
+  currentlyLoggedInUser
 }) {
-
   return (
     <li className="border border-gray-400 rounded-sm my-4">
       <div className="flex p-4 h-full items-center">
         <img src={thumbnailUrl} className="rounded-full mr-4 h-8 w-8" alt="" />
         <h3>{username}</h3>
-        <MdMoreHoriz size="1.5rem" className="ml-auto" />
+        <MdMoreHoriz className="ml-auto h-6 w-6" />
       </div>
       <img className="w-full" src={imageUrl} alt="" />
       <div className="p-4">
         <div>
           <div className="flex">
-            {doesCurrentUserLike 
-                ? (
-                  <FaHeart 
-                    onClick={toggleLike}
-                    size="1.7rem"
-                    className="fill-current text-red-600 mr-4 cursor-pointer"
-                  />
-                )
-                : (
-                  <FaRegHeart 
-                    onClick={toggleLike}
-                    size="1.7rem"
-                    className="mr-4 cursor-pointer text-gray-800"
-                  />
-                )
-            }
+            {doesCurrentUserLike ? (
+              <FaHeart
+                onClick={toggleLike}
+                className="fill-current text-red-600 mr-4 cursor-pointer h-6 w-6"
+              />
+            ) : (
+              <FaRegHeart
+                onClick={toggleLike}
+                className="mr-4 cursor-pointer text-gray-800 h-6 w-6"
+              />
+            )}
             <FaRegComment
-              size="1.7rem"
-              className="text-gray-800"
+              className="text-gray-800 h-6 w-6"
               style={{ transform: "rotateY(180deg)" }}
             />
           </div>
           <h4 className="font-semibold text-sm my-1">{likes} likes</h4>
         </div>
-        <CommentSection currentlyLoggedInUser={currentlyLoggedInUser} comments={comments} timestamp={timestamp} />
+        <CommentSection
+          currentlyLoggedInUser={currentlyLoggedInUser}
+          comments={comments}
+          timestamp={timestamp}
+        />
       </div>
     </li>
   );
@@ -66,5 +63,5 @@ Post.propTypes = {
   timestamp: PropTypes.string.isRequired,
   comments: PropTypes.arrayOf(PropTypes.shape(Comment.propTypes)),
   doesCurrentUserLike: PropTypes.bool.isRequired,
-  toggleLike: PropTypes.func.isRequired,
+  toggleLike: PropTypes.func.isRequired
 };
